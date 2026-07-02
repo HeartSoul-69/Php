6f3f3e5a7beb8e8412938784521d4bf6481cc196
HeartSoul-69 <anurag355singh@gmail.com>
2026-07-02 14:47:18 +0530

    Add files via upload

---
+++ Basics Php/include.php/Location.php
@@ -0,0 +1,23 @@
+<?php 
+    //include() = Copies the content of a file (php/html/text) and includes it in your php file.
+    //Sections of our website become reusable changes only need to be made in one place 
+
+    include("header.html");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    You are in Survival Mode
+</body>
+</html>
+
+<?php
+    include("footer.html");
+?>
